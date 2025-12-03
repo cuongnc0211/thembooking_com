00153ad9f4509f9761a0b9065847861dbece184c
cuongnc0211 <cuongnguyenfu@gmail.com>
2025-12-03 09:54:19 +0700

    enable solid queue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,10 +59,8 @@ Rails.application.configure do
   config.cache_store = :memory_store
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
-  # Solid Queue - DISABLED (temporarily)
-  # config.active_job.queue_adapter = :solid_queue
-  # config.solid_queue.connects_to = { database: { writing: :queue } }
-  config.active_job.queue_adapter = :async
+  config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
