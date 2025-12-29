ada844a7484fc7a4e716a0bdf85a226c1b90f733
cuongnc0211 <cuongnguyenfu@gmail.com>
2025-12-29 20:38:45 +0700

    fix db seed

--- config/routes.rb
+++ config/routes.rb
@@ -51,6 +51,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "/health#show", as: :rails_health_check
   get "/healthcheck", to: proc { [ 200, {}, [ "OK" ] ] }
+  get "/up", to: proc { [ 200, {}, [ "OK" ] ] }
 
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
